94d7d0769fa557a0e6f292337de0a234372cf7d1
mtaruno <matthew.taruno@gmail.com>
2022-01-10 20:07:25 +0700

    refactor and extra eda

--- utilities/linkedin_ingestion.py
+++ utilities/linkedin_ingestion.py
@@ -1,4 +1,4 @@
-'''
+"""
 Creates LinkedIn ingestion script
 
 * This scraper is an end-to-end script for scraping LinkedIn with Selenium.
@@ -15,7 +15,7 @@ Requirements:
 -------------
 A config.txt file containing your username and password (separated with a space)
 
-'''
+"""
 #%%
 import requests, time
 from datetime import datetime
@@ -31,78 +31,96 @@ from selenium.common.exceptions import NoSuchElementException
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support import expected_conditions as ec
 
+CONFIG_PATH = "/Users/mtaruno/Documents/DevZone/job-research/scraping/config.txt"
+CHROMEDRIVER_PATH = (
+    "/Users/mtaruno/Documents/DevZone/job-research/scraping/chromedriver-m1-15"
+)
+OUTPUT_PATH = "data/scraping_results/linkedin_"
 
 # Initializing account info
 # My personal login info (I put it into config.txt for privacy reasons)
-with open('../config.txt') as f:
+with open(CONFIG_PATH) as f:
     file = f.readlines()[0]
     username, password = file.split()
-    
+
 
 def initialize():
-    ''' Initializes a Chrome driver, opens linkedin and automatically inputs my username and password. '''
+    """ Initializes a Chrome driver, opens linkedin and automatically inputs my username and password. """
     # Input path to chrome driver executable
-    browser = webdriver.Chrome("../data/chromedriver-m1") # Now we're connected to a browser!
+    browser = webdriver.Chrome(CHROMEDRIVER_PATH)  # Now we're connected to a browser!
 
     # This driver allows us to access webpages from a chrome browser
     # Logging in to LinkedIn
-    browser.get('https://www.linkedin.com/login')
+    browser.get("https://www.linkedin.com/login")
 
     # Entering login info
-    elementID = browser.find_element_by_id('username')
+    elementID = browser.find_element_by_id("username")
     elementID.send_keys(username)
 
-    elementID = browser.find_element_by_id('password')
+    elementID = browser.find_element_by_id("password")
     elementID.send_keys(password)
 
     elementID.submit()
-    
+
     return browser
+
 
 browser = initialize()
 
 # Searching "data" in jobs
 
-browser.get('https://www.linkedin.com/jobs/?showJobAlertsModal=false')
-jobID = browser.find_element_by_class_name('jobs-search-box__text-input')
+browser.get("https://www.linkedin.com/jobs/?showJobAlertsModal=false")
+jobID = browser.find_element_by_class_name("jobs-search-box__text-input")
 jobID.send_keys("data")
 
 # Make sure to close messages first so that the search button is "clickable"
 # This code is an automated way of doing that
 
+
 def popup():
-    ''' Uses try loops to recognize when the popup is available and closing it
-    down in the event that it is. '''
+    """ Uses try loops to recognize when the popup is available and closing it
+    down in the event that it is. """
     try:
-        if browser.find_element_by_class_name('msg-overlay-list-bubble--is-minimized') is not None:
+        if (
+            browser.find_element_by_class_name("msg-overlay-list-bubble--is-minimized")
+            is not None
+        ):
             pass
     except NoSuchElementException:
         try:
-            if browser.find_element_by_class_name('msg-overlay-bubble-header') is not None:
-                browser.find_element_by_class_name('msg-overlay-bubble-header').click()
+            if (
+                browser.find_element_by_class_name("msg-overlay-bubble-header")
+                is not None
+            ):
+                browser.find_element_by_class_name("msg-overlay-bubble-header").click()
         except NoSuchElementException:
             pass
 
+
 popup()
 
-# Actually clicking the search button (we have to find the HTML element, 
+# Actually clicking the search button (we have to find the HTML element,
 # then use click method on that element)
-search = browser.find_element_by_class_name('jobs-search-box__submit-button')
+search = browser.find_element_by_class_name("jobs-search-box__submit-button")
 search.click()
 
 
-
 # Get page source code
-time.sleep(5) # While waiting for page to load
+time.sleep(5)  # While waiting for page to load
 
 src = browser.page_source
 
 # Beautiful Soup object
-soup = BeautifulSoup(src, 'lxml') # Using lxml parser
+soup = BeautifulSoup(src, "lxml")  # Using lxml parser
 # Make sure to do pip install lxml if you haven't
 
 # Get the search result number
-results = soup.find('small', {'class': 'display-flex t-12 t-black--light t-normal'}).get_text().strip().split()[0]
+results = (
+    soup.find("small", {"class": "display-flex t-12 t-black--light t-normal"})
+    .get_text()
+    .strip()
+    .split()[0]
+)
 results = f"There are {int(results.replace(',', ''))} results"
 print(results)
 
@@ -112,20 +130,26 @@ print(results)
 # Looking for all the job containers
 # I found the class from doing a string search for Umbel (which was my first job result in the chrome driver)
 
-job_container = soup.find_all('li', {"class":"jobs-search-results__list-item occludable-update p0 relative ember-view"})
+job_container = soup.find_all(
+    "li",
+    {
+        "class": "jobs-search-results__list-item occludable-update p0 relative ember-view"
+    },
+)
 
 # Filtering down to the links for individual companies
 expression = re.compile(r"\/jobs\/view")
-l1 = [job.attrs['href'] for job in soup.find_all('a')]
-postings = [ "https://linkedin.com" + s for s in l1 if expression.match(s) ]
-postings = list(set(postings)) # Getting unique postings
+l1 = [job.attrs["href"] for job in soup.find_all("a")]
+postings = ["https://linkedin.com" + s for s in l1 if expression.match(s)]
+postings = list(set(postings))  # Getting unique postings
+
 
 def scrape():
-    ''' Goes through postings one by one that was generated with the code before,
+    """ Goes through postings one by one that was generated with the code before,
     scrapes out all the desired information, and stores it in a dataframe.
     
     Returns the dataframe and saves it with the timestamp labeled on it as a csv file.
-    '''
+    """
     title = []
     description = []
     company_name = []
@@ -135,9 +159,9 @@ def scrape():
     time_posted = []
     employment_type = []
     applicant_count = []
-    
+
     for post in postings:
-    
+
         browser.get(post)
         popup()
         time.sleep(2)
@@ -159,14 +183,16 @@ def scrape():
         # Getting the title
         result = page.find_all("div", {"class": "p5"})
         title.append(result[0].h1.text)
-        
+
         # Getting the company name
         result = page.find_all("a", {"class": "ember-view t-black t-normal"})
-        company_name.append(result[0].text.replace("\n",""))
+        company_name.append(result[0].text.replace("\n", ""))
         try:
             # Getting the industry
-            result = page.find_all("li", {"class": "jobs-description-details__list-item t-14"})
-            industry.append(result[0].text.replace("\n",""))
+            result = page.find_all(
+                "li", {"class": "jobs-description-details__list-item t-14"}
+            )
+            industry.append(result[0].text.replace("\n", ""))
         except:
             industry.append(np.nan)
         try:
@@ -174,42 +200,55 @@ def scrape():
             job_functions.append(result[1].text.replace("\n", ""))
         except:
             job_functions.append(np.nan)
-        
+
         # Getting the location
         result = page.find_all("span", {"class": "jobs-unified-top-card__bullet"})
         location.append(result[0].text)
-        
+
         try:
             # Getting the employment type
             result = page.find_all("p", {"class": "t-14 mb3"})
             employment_type.append(result[0].text.replace("\n", ""))
         except:
             employment_type.append(np.nan)
-        
+
         # Getting the time posted
         try:
-            result = page.find_all("span", {"class": "jobs-unified-top-card__posted-date"})
+            result = page.find_all(
+                "span", {"class": "jobs-unified-top-card__posted-date"}
+            )
             time_posted.append(result[0].text.replace("\n", ""))
         except:
             time_posted.append(np.nan)
-        
+
         # Getting the applicant count
         try:
-            result = page.find_all("span", {"class": "jobs-unified-top-card__applicant-count"})
+            result = page.find_all(
+                "span", {"class": "jobs-unified-top-card__applicant-count"}
+            )
             applicant_count.append(result[0].text.replace("\n", ""))
         except:
             applicant_count.append(np.nan)
-    
+
     # Storing in dataframe
-    df = pd.DataFrame({"Title": title, "Description": description,
-                      "Company Name": company_name, "Location": location,
-                      "Industry": industry, "Job Functions": job_functions, 
-                      "Time Posted": time_posted, "Employment Type": employment_type,
-                      "Applicant Count": applicant_count})
-    
+    df = pd.DataFrame(
+        {
+            "Title": title,
+            "Description": description,
+            "Company Name": company_name,
+            "Location": location,
+            "Industry": industry,
+            "Job Functions": job_functions,
+            "Time Posted": time_posted,
+            "Employment Type": employment_type,
+            "Applicant Count": applicant_count,
+        }
+    )
+
     # Saving dataframe into csv, with a timestamp attached to it
-    df.to_csv("data/scraping_results/linkedin_" + str(datetime.now())[:19] + ".csv")
-    
+    df.to_csv(OUTPUT_PATH + str(datetime.now())[:19] + ".csv")
+
     return df
+
 
 df = scrape()
